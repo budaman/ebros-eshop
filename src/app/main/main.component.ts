6edd2ad285c648ea97362ac98374dcb4d1675a83
budaman <budamantvydas@gmail.com>
2017-09-12 13:33:01 +0300

    slides works well

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,32 +7,126 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  currentPage:number
+  photosPerPage:number
+  indexOfLastPhoto:number
+  indexOfFirstPhoto:number
+  pagesAtAll:number
+  currentPhotos:any[]
+  selectedPhoto:number
+  name:string
+  age:number
+  email:string
+  address:Address
+  hobbies:string[]
+  hobbies1:any[] //can make array
+
+  constructor() {
+    console.log('contructor')
+   }
 
   ngOnInit() {
+    this.selectedPhoto = 1
+    this.photosPerPage = 3
+    this.currentPage = 1
+    this.name = 'Mantvydas'
+    this.age = 30
+    this.address = {
+      street: 'kovo11',
+      city: 'kaunas',
+      state: 'whatever'
+    }
+    this.hobbies =['Write code', 'Watch movies', 'Listen to music']
+    this.indexOfLastPhoto = this.currentPage * this.photosPerPage
+    this.indexOfFirstPhoto = this.indexOfLastPhoto -  this.photosPerPage
+    this.currentPhotos = this.photos.slice(this.indexOfFirstPhoto, this.indexOfLastPhoto)
   }
 
+arrowPush(e:any) {
+  this.pagesAtAll = this.photos.length / this.photosPerPage
+  this.pagesAtAll = Math.ceil(this.pagesAtAll)
+  if(e.target.id == 'up' && this.currentPage < this.pagesAtAll) {
+    this.currentPage++
+  }
+  if(e.target.id =='down' && this.currentPage > 1) {
+    this.currentPage--
+  }
+  this.indexOfLastPhoto = this.currentPage * this.photosPerPage
+  this.indexOfFirstPhoto = this.indexOfLastPhoto -  this.photosPerPage
+  this.currentPhotos = this.photos.slice(this.indexOfFirstPhoto, this.indexOfLastPhoto)
+}
+
+selected (id:number) {
+  this.selectedPhoto = id
+}
+
+
+// photosPerPage: 3
+// indexOfLastPhoto = this.currentPage * this.photosPerPage
+// indexOfFirstCoord = this.indexOfLastPhoto- this.photosPerPage
+// currentPhotos = this.photos.slice(this.indexOfFirstCoord, this.indexOfLastPhoto)
 photos = [
-  {'photo' : 1},
-  {'photo' : 2},
-  {'photo' : 3},
-  {'photo' : 4},
-  {'photo' : 5},
-  {'photo' : 6},
-  {'photo' : 7},
-  {'photo' : 8},
-  {'photo' : 9},
-  {'photo' : 10},
-  {'photo' : 11},
-  {'photo' : 12},
-  {'photo' : 13},
-  {'photo' : 14}
+  {'small' : './assets/small-1.jpg',
+    'big'  : './assets/1.jpg',
+    'id' : 1
+},
+  {'small' : './assets/small-2.jpg',
+    'big'  : './assets/2.jpg',
+    'id' : 2
+},
+  {'small' : './assets/small-3.jpg',
+    'big'  : './assets/3.jpg',
+    'id' : 3
+},
+  {'small' : './assets/small-4.jpg',
+    'big'  : './assets/4.jpg',
+    'id' : 4
+},
+  {'small' : './assets/small-5.jpg',
+    'big'  : './assets/5.jpg',
+    'id' : 5
+},
+  {'small' : './assets/small-6.jpg',
+    'big'  : './assets/6.jpg',
+    'id' : 6
+},
+  {'small' : './assets/small-7.jpg',
+    'big'  : './assets/7.jpg',
+    'id' : 7
+},
+  {'small' : './assets/small-8.jpg',
+    'big'  : './assets/8.jpg',
+    'id' : 8
+},
+  {'small' : './assets/small-9.jpg',
+    'big'  : './assets/9.jpg',
+    'id' : 9
+},
+  {'small' : './assets/small-10.jpg',
+    'big'  : './assets/10.jpg',
+    'id' : 10
+},
+  {'small' : './assets/small-11.jpg',
+    'big'  : './assets/11.jpg',
+    'id' : 11
+},
+  {'small' : './assets/small-12.jpg',
+    'big'  : './assets/12.jpg',
+    'id' : 12
+},
+  {'small' : './assets/small-13.jpg',
+    'big'  : './assets/13.jpg',
+    'id' : 13
+},
+  {'small' : './assets/small-14.jpg',
+    'big'  : './assets/14.jpg',
+    'id' : 14
+}
 ]
+}
 
-currentPage: 1
-photosPerPage: 3
-indexOfLastPhoto = this.currentPage * this.photosPerPage
-indexOfFirstCoord = this.indexOfLastPhoto- this.photosPerPage
-currentPhotos = this.photos.slice(this.indexOfFirstCoord, this.indexOfLastPhoto)
-
+interface Address {
+  street:string,
+  city:string,
+  state:string
 }
